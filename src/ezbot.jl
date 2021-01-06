6de6950cc8ff82921870856aa68c0f3925af3e70
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-01-06 15:03:09 -0300

    updated Project.toml

--- src/ezbot.jl
+++ src/ezbot.jl
@@ -1,25 +1,16 @@
 module ezbot
 
-    using Indicators
-    using PyCall
-    using Formatting
-    using IterTools
-    using ScikitLearn
-    using PyPlot
     using Dates
-
-    include("marketoverview.jl")
-    include("toplevel.jl")
-    include("midlevel.jl")
-    include("lowlevel.jl")
-
-
-    ScikitLearn.Skcore.import_sklearn() = PyCall.pyimport_conda("sklearn", "scikit-learn")
+    using Formatting
+    using Indicators
+    using IterTools
+    using PyCall
+    using PyPlot
+    using ScikitLearn
 
     @sk_import preprocessing:MinMaxScaler
     @sk_import neural_network:BernoulliRBM
     @sk_import manifold:SpectralEmbedding
-
 
     @pyimport binance.client as bnc
     @pyimport binance.websockets as bws
@@ -28,10 +19,15 @@ module ezbot
     @pyimport urllib3
     urllib3.disable_warnings()
 
+    include("marketoverview.jl")
+    include("toplevel.jl")
+    include("midlevel.jl")
+    include("lowlevel.jl")
+
     api_key = ""
     api_secret = ""
 
-    global ibtc, client
+    #global ibtc, client
     ibtc = 0.0012
     client = bnc.Client(api_key, api_secret, Dict([:"verify" => false, :"timeout" => 90]))
 
