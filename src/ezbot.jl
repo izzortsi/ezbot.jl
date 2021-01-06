a7831c7d5cd413f574b1e1d040ddc8169e262c11
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-01-06 15:30:13 -0300

    more fixes

--- src/ezbot.jl
+++ src/ezbot.jl
@@ -1,39 +1,39 @@
 module ezbot
 
-    using Dates
-    using Formatting
-    using Indicators
-    using IterTools
-    using PyCall
-    using PyPlot
-    using ScikitLearn
+using Dates
+using Formatting
+using Indicators
+using IterTools
+using PyCall
+using PyPlot
+using ScikitLearn
 
-    @sk_import preprocessing:MinMaxScaler
-    @sk_import neural_network:BernoulliRBM
-    @sk_import manifold:SpectralEmbedding
+@sk_import preprocessing:MinMaxScaler
+@sk_import neural_network:BernoulliRBM
+@sk_import manifold:SpectralEmbedding
 
-    @pyimport binance.client as bnc
-    @pyimport binance.websockets as bws
-    @pyimport binance.enums as ben 
+@pyimport binance.client as bnc
+@pyimport binance.websockets as bws
+@pyimport binance.enums as ben 
 
-    @pyimport urllib3
-    urllib3.disable_warnings()
+@pyimport urllib3
+urllib3.disable_warnings()
 
-    include("marketoverview.jl")
-    include("toplevel.jl")
-    include("midlevel.jl")
-    include("lowlevel.jl")
+include("marketoverview.jl")
+include("toplevel.jl")
+include("midlevel.jl")
+include("lowlevel.jl")
 
-    api_key = ""
-    api_secret = ""
+api_key = ""
+api_secret = ""
 
-    #global ibtc, client
-    ibtc = 0.0012
-    client = bnc.Client(api_key, api_secret, Dict([:"verify" => false, :"timeout" => 90]))
+#global ibtc, client
+ibtc = 0.0012
+client = bnc.Client(api_key, api_secret, Dict([:"verify" => false, :"timeout" => 90]))
 
-    pygui(true)
+pygui(true)
 
-    export client, ibtc, overlook, look, play
+export client, ibtc, overlook, look, play
 
 end
 
