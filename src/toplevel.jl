636a54c6bc1c578ee126e215cc456e808e94fa47
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-01-06 16:36:08 -0300

    more fixes

--- src/toplevel.jl
+++ src/toplevel.jl
@@ -1,7 +1,7 @@
 pygui(true)
 
 function initialize(; api_key = "", api_secret = "", ibtc = 0.0012)
-    return bnc.Client(api_key, api_secret, Dict([:"verify" => false, :"timeout" => 90]))
+    copy!(client, bnc.Client(api_key, api_secret, Dict([:"verify" => false, :"timeout" => 90])))
 end
 
 """
